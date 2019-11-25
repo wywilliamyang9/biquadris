32da897f85d7fb6bce1fec89c43a8c6abd90a45d
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-24 19:18:50 -0500

    hi

--- biquadris/game.h
+++ biquadris/game.h
@@ -1,1 +1,16 @@
-#ifndef Game.h
+#ifndef _GAME_
+#define _GAME_
+#include "board.h"
+class Game {
+    Board board1;
+    Board board2;
+    int score1;
+    int score2;
+    int hi;
+
+};
+
+
+
+
+#endif
---
+++ biquadris/subject.h
@@ -0,0 +1,18 @@
+#ifndef _OBSERVER_
+#define _OBSERVER_
+
+template <typename StateType> class Observer;
+
+template <typename StateType> class Subject {
+    std::vector<Observer<StateType>*> observers;
+    StateType state;
+
+    protected:
+    void setState(StateType newS);
+
+    public:
+    void attach(Observer<StateType> *o);
+    virtual ~Observer() = default;
+};
+
+#endif
