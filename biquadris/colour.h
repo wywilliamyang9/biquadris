54c3e419d00415a9e7e01b8b3e948106cf21e639
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-25 16:28:32 -0500

    /

---
+++ biquadris/colour.h
@@ -0,0 +1,7 @@
+#ifndef _COLOUR_
+#define _COLOUR_
+
+enum class Colour {Red, Orange, Yellow, 
+Green, Blue, Purple, Black, Uncoloured}
+
+#endif
---
+++ biquadris/commands.h
@@ -0,0 +1,12 @@
+#ifndef _COMMANDS_
+#define _COMMANDS_
+enum class Commands {
+    Left, Right, Down, ClockWise, CounterClockWise, Drop,
+    LevelUp, LevelDown, NoRandom, Random, Sequence, I, J,
+    L, O, S, Z, T,
+- Restart
+- Blind
+- Heavy
+- Force
+}
+#endif
