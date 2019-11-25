6b004dae5b3649199c4775086180c56c0075e4dc
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-25 12:34:01 -0500

    /

--- biquadris/board.h
+++ biquadris/board.h
@@ -2,13 +2,22 @@
 #define _BOARD_
 #include "block.h"
 #include "specialAction.h"
+#include "level.h"
+#include "graphicDisplay.h"
 #include <vector>
+#include <fstream>
 
 class Board {
     Board* opponent;
     Vector<Vector<Cell>> board;
-    
-    
+    Vector<SpecialAction> specialActions;
+    Level level;
+    GraphicDisplay graphicDisplay;
+    TextDisplay textDisplay;
+    fstream filesream;
+
+    public:
+    int clearRow(); // clears filled rows, 
 }
 
 #endif
