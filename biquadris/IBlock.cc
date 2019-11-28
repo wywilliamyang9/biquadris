daff602e7a551411bc6b252696fe0392b1847e77
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-28 18:19:00 -0500

    /

--- biquadris/IBlock.cc
+++ biquadris/IBlock.cc
@@ -1,75 +1,39 @@
 #include "IBlock.h"
-IBlock::IBlock (Cell& c1, Cell& c2, Cell& c3, Cell& c4,
+IBlock::IBlock (Cell* c1, Cell* c2, Cell* c3, Cell* c4,
 int heavy, Colour colour): Block{c1,c2,c3,c4,heavy,colour} {}
 
 void IBlock::CWRotate(Board & playerBoard) {
     vector<vector<Cells>>& board = playerBoard.getBoard();
     if (state == 1) {
-        Info i0 = cells.at(0).getinfo();
-        board.at(i0.coordinates.row-3,i0.coordinates.col).setInfo(
-            Coordinate {i0.coordinates.row-3,i0.coordinates.col}
-            i0.colour;
-            i0.blinded;
-            i0.boardnum;
-        );
-        cells.at(0).setInfo(
-            i0.coordinates;
-            Colour::White;
-            i0.blinded;
-            i0.boardnum;
-        );
+        Info i0 = cells.at(0)->getinfo();
+        playerBoard.getBoard().at(i0.coordinates.row-3,i0.coordinates.col).
+        setColour(i0.colour);
+        cells.at(0)->setColour(Colour::White);
 
-        Info i1 = cells.at(1).getinfo();
-        board.at(i1.coordinates.row-2,i1.coordinates.col-1).setInfo(
-            Coordinate {i1.coordinates.row-2,i1.coordinates.col-1};
-            i1.colour;
-            i1.blinded;
-            i1.boardnum;
-        ); 
-        cells.at(1).setInfo(
-            i1.coordinates;
-            Colour::White;
-            i1.blinded;
-            i1.boardnum;
-        );
+        Info i1 = cells.at(1)->getinfo();
+        playerBoard.getBoard().at(i1.coordinates.row-2,i1.coordinates.col-1).
+        setColour(i1.colour); 
+        cells.at(1)->setColour(Colour::White);
      
-        Info i2 = cells.at(2).getinfo();
-        board.at(i2.coordinates.row-1,i2.coordinates.col-2).setInfo(
-            Coordinate {i2.coordinates.row-1,i2.coordinates.col-2};
-            i2.colour;
-            i2.blinded;
-            i2.boardnum;
-        ); 
-        cells.at(2).setInfo(
-            i2.coordinates;
-            Colour::White;
-            i2.blinded;
-            i2.boardnum;
-        );
+        Info i2 = cells.at(2)->getinfo();
+        playerBoard.getBoard().at(i2.coordinates.row-1,i2.coordinates.col-2).
+        setColour(i2.colour); 
+        cells.at(2)->setColour(Colour::White);
 
-        Info i3 = cells.at(3).getinfo();
-        board.at(i3.coordinates.row,i3.coordinates.col-3).setInfo(
-            Coordinate {i3.coordinates.row,i3.coordinates.col-3};
-            i3.colour;
-            i3.blinded;
-            i3.boardnum;
-        ); 
-        cells.at(3).setInfo(
-            i3.coordinates;
-            Colour::White;
-            i3.blinded;
-            i3.boardnum;
-        );
+        Info i3 = cells.at(3)->getinfo();
+        playerBoard.getBoard().at(i3.coordinates.row,i3.coordinates.col-3).
+        setColour(i3.colour); 
+        cells.at(3)->setColour(Colour::White);
         
-        vector<Cells&> newCells;
+        vector<Cells*> newCells;
         newCells.emplace_back(
-            board.at(i0.coordinates.row-3,i0.coordinates.col)); 
+            playerBoard.getBoard().at(i0.coordinates.row-3,i0.coordinates.col)); 
         newCells.emplace_back(
-            board.at(i1.coordinates.row-2,i1.coordinates.col-1)); 
+            playerBoard.getBoard().at(i1.coordinates.row-2,i1.coordinates.col-1)); 
         newCells.emplace_back(
-            board.at(i2.coordinates.row-1,i2.coordinates.col-2));
+            playerBoard.getBoard().at(i2.coordinates.row-1,i2.coordinates.col-2));
         newCells.emplace_back(
-            board.at(i3.coordinates.row,i3.coordinates.col-3));
+            playerBoard.getBoard().at(i3.coordinates.row,i3.coordinates.col-3));
         
         cells.clear();
         cells = newCells;
@@ -80,69 +44,35 @@ void IBlock::CWRotate(Board & playerBoard) {
         if (i0.coordinates(col)+3 > 17) return;
 
         // if rotation is legit.
-        Info i3 = cells.at(3).getinfo();
-        board.at(i3.coordinates.row,i3.coordinates.col+3).setInfo(
-            Coordinate {i3.coordinates.row,i3.coordinates.col+3};
-            i3.colour;
-            i3.blinded;
-            i3.boardnum;
-        ); 
-        cells.at(3).setInfo(
-            i3.coordinates;
-            Colour::White;
-            i3.blinded;
-            i3.boardnum;
-        );
+        Info i3 = cells.at(3)->getinfo();
+        playerBoard.getBoard().at(i3.coordinates.row,i3.coordinates.col+3).
+        setColour(i3.colour);
+        cells.at(3)->setColour(Colour::White);
 
-        Info i2 = cells.at(2).getinfo();
-        board.at(i2.coordinates.row+1,i2.coordinates.col+2).setInfo(
-            Coordinate {i2.coordinates.row+1,i2.coordinates.col+2};
-            i2.colour;
-            i2.blinded;
-            i2.boardnum;
-        ); 
-        cells.at(2).setInfo(
-            i2.coordinates;
-            Colour::White;
-            i2.blinded;
-            i2.boardnum;
-        );
+        Info i2 = cells.at(2)->getinfo();
+        playerBoard.getBoard().at(i2.coordinates.row+1,i2.coordinates.col+2).
+        setColour(i2.colour); 
+        cells.at(2)->setColour(Colour::White);
 
-        Info i1 = cells.at(1).getinfo();
-        board.at(i1.coordinates.row+2,i1.coordinates.col+1).setInfo(
-            Coordinate {i1.coordinates.row+2,i1.coordinates.col+1};
-            i1.colour;
-            i1.blinded;
-            i1.boardnum;
-        ); 
-        cells.at(1).setInfo(
-            i1.coordinates;
-            Colour::White;
-            i1.blinded;
-            i1.boardnum;
-        );
-        board.at(i0.coordinates.row+3,i0.coordinates.col).setInfo(
-            Coordinate {i0.coordinates.row+3,i0.coordinates.col}
-            i0.colour;
-            i0.blinded;
-            i0.boardnum;
-        );
-        cells.at(0).setInfo(
-            i0.coordinates;
-            Colour::White;
-            i0.blinded;
-            i0.boardnum;
-        );
+        Info i1 = cells.at(1)->getinfo();
+        playerBoard.getBoard().at(i1.coordinates.row+2,i1.coordinates.col+1).
+        setColour(i1.colour); 
+        cells.at(1)->setColour(Colour::White);
+
+        Info i0 = cells.at(1)->getinfo();
+        playerBoard.getBoard().at(i0.coordinates.row+3,i0.coordinates.col).
+        setColour(i0.colour);
+        cells.at(0)->setColour(Colour::White);
 
         vector<Cells&> newCells;
         newCells.emplace_back(
-            board.at(i0.coordinates.row+3,i0.coordinates.col)); 
+            playerBoard.getBoard().at(i0.coordinates.row+3,i0.coordinates.col)); 
         newCells.emplace_back(
-            board.at(i1.coordinates.row+2,i1.coordinates.col+1)); 
+            playerBoard.getBoard().at(i1.coordinates.row+2,i1.coordinates.col+1)); 
         newCells.emplace_back(
-            board.at(i2.coordinates.row+1,i2.coordinates.col+2));
+            playerBoard.getBoard().at(i2.coordinates.row+1,i2.coordinates.col+2));
         newCells.emplace_back(
-            board.at(i3.coordinates.row,i3.coordinates.col+3));
+            playerBoard.getBoard().at(i3.coordinates.row,i3.coordinates.col+3));
         
         cells.clear();
         cells = newCells;
--- biquadris/block.cc
+++ biquadris/block.cc
@@ -24,13 +24,12 @@ Block::Block(Cell* c1, Cell* c2, Cell* c3, Cell* c4,
 
 void Block::moveDownByOne(Board& playerBoard) {
 	bool canMove = 1;
-	vector<vector<Cell>> board = playerBoard.getBoard();
 
 	for (int i = 0; i < 4; ++i) {
-		Coordinates curCoord = cells.at(i).getinfo().coord;
-		if (board.at(curCoord.row + 1).at(curCoord.col).getinfo().colour
+		Coordinates curCoord = cells.at(i)->getinfo().coord;
+		if (playerBoard.getBoard().at(curCoord.row + 1).at(curCoord.col).getinfo().colour
 			!= Colour::White
-			&& board.at(curCoord.row + 1).at(curCoord.col).getCurrBlock
+			&& playerBoard.getBoard().at(curCoord.row + 1).at(curCoord.col).getCurrBlock
 			== false) canMove = 0;
 	}
 
@@ -39,10 +38,10 @@ void Block::moveDownByOne(Board& playerBoard) {
 		
 		for (int i = 0; i < 4; ++i) {
 			Cell* oldCell = cells.at(i);
-			Coordinates currCoord = oldCell.getinfo().coord;
-			Cell* newCell = &(board.at(currCoord.row + 1).at(currCoord.col));
+			Coordinates currCoord = oldCell->getinfo().coord;
+			Cell* newCell = &(playerBoard.getBoard().at(currCoord.row + 1).at(currCoord.col));
 			// Change target cell (one cell down)
-			newCell->setColour(oldCell.getinfo().colour);
+			newCell->setColour(oldCell->getinfo().colour);
 			newCell->setCurrBlock(true);
 
 			// change current cell
@@ -62,30 +61,29 @@ void Block::moveDown(Board& playerBoard) {
 
 void Block::moveLeft(Board& playerBoard) {
 	bool canMove = 1;
-	vector<vector<Cell>> board = playerBoard.getBoard();
 
 	//check if it can go to left by 1 column.
 	for (int i = 0; i < 4; ++i) {
-		Coordinates curCoord = cells.at(i).getinfo().coord;
+		Coordinates curCoord = cells.at(i)->getinfo().coord;
 		if (curCoord.col == 0 // if already first col
-			|| (board.at(curCoord.row).at(curCoord.col - 1).getinfo().colour
-				!= Colour::White && (board.at(curCoord.row).at(curCoord.col - 1).getCurrBlock()
+			|| (playerBoard.getBoard().at(curCoord.row).at(curCoord.col - 1).getinfo().colour
+				!= Colour::White && (playerBoard.getBoard().at(curCoord.row).at(curCoord.col - 1).getCurrBlock()
 					== false))) canMove = 0;
 	}
 
 	if (canMove) {
 		vector<Cell&> newCells;
 		for (int i = 0; i < 4; ++i) {
-			Cell& oldCell = cells.at(i);
-			Coordinates currCoord = oldCell.getinfo().coord;
-			Cell& newCell = board.at(currCoord.row).at(currCoord.col - 1);
+			Cell* oldCell = cells.at(i);
+			Coordinates currCoord = oldCell->getinfo().coord;
+			Cell* newCell = &(playerBoard.getBoard().at(currCoord.row).at(currCoord.col - 1));
 			// Change target cell (one cell down)
-			newCell.setColour(oldCell.getinfo().colour);
-			newCell.setCurrBlock(true);
+			newCell->setColour(oldCell->getinfo().colour);
+			newCell->setCurrBlock(true);
 
 			// change current cell
-			oldCell.setColour(Colour::White);
-			newCell.setCurrBlock(false);
+			oldCell->setColour(Colour::White);
+			newCell->setCurrBlock(false);
 			// get new cell
 			newCells.emplace_back(newCell);
 		}
@@ -93,34 +91,34 @@ void Block::moveLeft(Board& playerBoard) {
 		cells = newCells;
 	}
 }
+
 void Block::moveRight(Board& playerBoard) {
 	bool canMove = 1;
-	vector<vector<Cell>> board = playerBoard.getBoard();
 
 	//check if it can go to left by 1 column.
 	for (int i = 0; i < 4; ++i) {
-		Coordinates curCoord = cells.at(i).getinfo().coord;
+		Coordinates curCoord = cells.at(i)->getinfo().coord;
 		if (curCoord.col == 17 // if already first col
-			|| (board.at(curCoord.row).at(curCoord.col + 1).getinfo().colour
-				!= Colour::White && (board.at(curCoord.row).at(curCoord.col + 1).getCurrBlock()
+			|| (playerBoard.getBoard().at(curCoord.row).at(curCoord.col + 1).getinfo().colour
+				!= Colour::White && (playerBoard.getBoard().at(curCoord.row).at(curCoord.col + 1).getCurrBlock()
 					== false))) canMove = 0;
 	}
 
 	if (canMove) {
 		vector<Cell&> newCells;
 		for (int i = 0; i < 4; ++i) {
-			Cell& oldCell = cells.at(i);
-			Coordinates currCoord = oldCell.getinfo().coord;
-			Cell& newCell = board.at(currCoord.row).at(currCoord.col + 1);
+			Cell* oldCell = cells.at(i);
+			Coordinates currCoord = oldCell->getinfo().coord;
+			Cell* newCell = playerBoard.getBoard().at(currCoord.row).at(currCoord.col + 1);
 			// Change target cell (one cell down)
-			newCell.setColour(oldCell.getinfo().colour);
-			newCell.setCurrBlock(true);
+			newCell->setColour(oldCell->getinfo().colour);
+			newCell->setCurrBlock(true);
 
 			// change current cell
-			oldCell.setColour(Colour::White);
-			newCell.setCurrBlock(false);
+			oldCell->setColour(Colour::White);
+			newCell->setCurrBlock(false);
 			// get new cell
-			newCells.emplace_back(newCell);
+			newCells->emplace_back(newCell);
 		}
 		for (int i = 0; i < heavy; ++i) moveDownByOne(playerBoard);
 		cells = newCells;
