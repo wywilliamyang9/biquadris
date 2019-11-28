2a4f54b082e0456656e5e0274b622c5ffb575ff6
s368yang <s368yang@student.cs.uwaterloo.ca>
2019-11-27 22:04:25 -0500

    /

---
+++ biquadris/level.cc
@@ -0,0 +1,56 @@
+#include "level.h"
+#include "block.h"
+#include "cell.h"
+#include "colour.h"
+
+using namespace std;
+
+Level::Level(int seed, int blocknum): seed{seed}, blocknum{blocknum}{}
+
+void Level::forceBlock(Colour forcedBlock){
+    nextBlock = forceBlock;
+}
+
+void Level::addHeavy(){
+    heavy++;
+}
+
+int Level::getLevel(){
+    return level;
+}
+
+Colour Level::getNextBlock(){
+    return nextBlock;
+}
+
+Block* Level::createBlock(){
+    if (convertColour(nextBlock) == 'I'){
+        Block *block = new Block{Cell{3, 0, blocknum}, Cell{3, 1, blocknum}, Cell{3, 2, blocknum}, Cell{3, 3, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'J'){
+        Block *block = new Block{Cell{2, 0, blocknum}, Cell{3, 0, blocknum}, Cell{3, 1, blocknum}, Cell{3, 2, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'L'){
+        Block *block = new Block{Cell{2, 2, blocknum}, Cell{3, 2, blocknum}, Cell{3, 1, blocknum}, Cell{3, 0, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'O'){
+        Block *block = new Block{Cell{2, 0, blocknum}, Cell{2, 1, blocknum}, Cell{3, 0, blocknum}, Cell{3, 1, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'S'){
+        Block *block = new Block{Cell{3, 0, blocknum}, Cell{3, 1, blocknum}, Cell{2, 1, blocknum}, Cell{2, 2, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'J'){
+        Block *block = new Block{Cell{2, 0, blocknum}, Cell{2, 1, blocknum}, Cell{3, 1, blocknum}, Cell{3, 2, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    } else if (convertColour(nextBlock) == 'J'){
+        Block *block = new Block{Cell{2, 0, blocknum}, Cell{2, 1, blocknum}, Cell{2, 2, blocknum}, Cell{3, 1, blocknum}, heavy, nextBlock};
+        heavy = 0;
+        return block;
+    }
+}
