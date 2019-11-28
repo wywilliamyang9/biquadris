e60171ae1d2c292ab54d785d5a36535aa883c36a
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-28 17:47:13 -0500

    /

--- biquadris/level.cc
+++ biquadris/level.cc
@@ -23,34 +23,3 @@ Colour Level::getNextBlock(){
     return nextBlock;
 }
 
-Block* Level::createBlock(){
-    if (convertColour(nextBlock) == 'I'){
-        Block *block = new Block{make_share<Cell>(3, 0, blocknum), make_share<Cell>(3, 1, blocknum), make_share<Cell>(3, 2, blocknum), make_share<Cell>(3, 3, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'J'){
-        Block *block = new Block{make_share<Cell>(2, 0, blocknum), make_share<Cell>(3, 0, blocknum), make_share<Cell>(3, 1, blocknum), make_share<Cell>(3, 2, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'L'){
-        Block *block = new Block{make_share<Cell>(2, 2, blocknum), make_share<Cell>(3, 2, blocknum), make_share<Cell>(3, 1, blocknum), make_share<Cell>(3, 0, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'O'){
-        Block *block = new Block{make_share<Cell>(2, 0, blocknum), make_share<Cell>(2, 1, blocknum), make_share<Cell>(3, 0, blocknum), make_share<Cell>(3, 1, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'S'){
-        Block *block = new Block{make_share<Cell>(3, 0, blocknum), make_share<Cell>(3, 1, blocknum), make_share<Cell>(2, 1, blocknum), make_share<Cell>(2, 2, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'J'){
-        Block *block = new Block{make_share<Cell>(2, 0, blocknum), make_share<Cell>(2, 1, blocknum), make_share<Cell>(3, 1, blocknum), make_share<Cell>(3, 2, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    } else if (convertColour(nextBlock) == 'J'){
-        Block *block = new Block{make_share<Cell>(2, 0, blocknum), make_share<Cell>(2, 1, blocknum), make_share<Cell>(2, 2, blocknum), make_share<Cell>(3, 1, blocknum), heavy, nextBlock};
-        heavy = 0;
-        return block;
-    }
-}
