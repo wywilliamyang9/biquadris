0e46e40b33c87be64f900787bf84fcd9d1352b6f
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-11-27 21:58:46 -0500

    /

---
+++ biquadris/OBlock.h
@@ -0,0 +1,14 @@
+#ifndef _OBLOCK_
+#define _IBLOCK_
+#include "block.h"
+
+class IBlock: public Block {
+    public:
+    IBlock (Cell&, Cell&, Cell&, Cell&,
+        int heavy, Colour);
+
+    void CWRotate(Board &)override;
+    void CounterCWRotate(Board &)override;
+}
+
+#endif
