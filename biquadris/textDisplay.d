b087103566fc1e4123f6ef962c0bd8b6dd74270f
s368yang <s368yang@student.cs.uwaterloo.ca>
2019-11-30 17:14:40 -0500

    /

--- biquadris/textDisplay.d
+++
@@ -1,5 +0,0 @@
-textDisplay.o: textDisplay.cc textDisplay.h subject.h info.h colour.h \
- coordinates.h nextBlock.h observer.h board.h level.h blockInfo.h \
- level0.h level1.h level2.h level3.h specialAction.h commandInterpreter.h \
- command.h cell.h IBlock.h block.h JBlock.h LBlock.h OBlock.h TBlock.h \
- SBlock.h ZBlock.h
