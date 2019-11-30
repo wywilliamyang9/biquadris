724017ad5b9837663ed4cf1fc40b91a3f29f8d25
s368yang <s368yang@student.cs.uwaterloo.ca>
2019-11-30 14:06:07 -0500

    /

---
+++ biquadris/game.d
@@ -0,0 +1,4 @@
+game.o: game.cc game.h board.h level.h colour.h subject.h info.h \
+ coordinates.h blockInfo.h level0.h level1.h observer.h specialAction.h \
+ commandInterpreter.h command.h nextBlock.h textDisplay.h cell.h IBlock.h \
+ block.h
---
+++ biquadris/level2.d
@@ -0,0 +1,2 @@
+level2.o: level2.cc level2.h blockInfo.h colour.h level.h subject.h \
+ info.h coordinates.h block.h cell.h observer.h
