039d1d17fe71ed57eb34849a3a917616c7d0f120
c267huan <c267huan@student.cs.uwaterloo.ca>
2019-12-02 17:06:33 -0500

    /

---
+++ biquadris/test.cc
@@ -0,0 +1,14 @@
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	char* a{"abcdeoiajpdofa;sdf;asdnf;nalksfnlkasndflknasjkdfndffgasdflkjnalksfklanwelkfnlkawenfknawlkenflkasndfjnklasfj"};
+    string b{"hi"};
+    //a = b;
+	cout << sizeof(char) << endl;
+}
+
+
