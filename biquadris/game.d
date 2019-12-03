6246209c0d0dafc9bd404872081fa461a89b2819
s368yang <s368yang@student.cs.uwaterloo.ca>
2019-12-03 09:39:26 -0500

    .

--- biquadris/game.d
+++
@@ -1,4 +0,0 @@
-game.o: game.cc game.h board.h level.h colour.h blockInfo.h level0.h \
- level1.h level2.h level3.h level4.h subject.h info.h coordinates.h \
- observer.h nextBlock.h specialAction.h commandInterpreter.h command.h \
- cell.h block.h textDisplay.h graphicalDisplay.h window.h
--- biquadris/graphicalDisplay.d
+++
@@ -1,2 +0,0 @@
-graphicalDisplay.o: graphicalDisplay.cc window.h graphicalDisplay.h \
- subject.h info.h colour.h coordinates.h nextBlock.h observer.h
